9b19b87dbdf1848b7261098ea1551fe425768032
macdre <ross.e.macd@gmail.com>
2019-05-24 19:04:18 -0300

    updating hash

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,7 +2,7 @@
 # browser auto-opening
 # Set ip to '*' to bind on all interfaces (ips) for the public server
 c.NotebookApp.ip = '*'
-c.NotebookApp.password = u'dev'
+c.NotebookApp.password = u'sha1:3cbd4e1d0205:30d5570ddabf373588fe7599554a326996e77245'
 c.NotebookApp.open_browser = False
 
 # It is a good idea to set a known, fixed port for server access
